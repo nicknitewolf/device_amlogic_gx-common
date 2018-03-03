1d78e46d285f1d3736fb948aaad21597e57febe0
Kevin Rocard <krocard@google.com>
2018-03-02 19:41:31 -0800

    gx: Uprev audio to 4.0
    Test: regression test on the board
    Bug: 38184704
    Change-Id: Ife559125850277865451f92ea862e7ec47fc9da7
    Merged-In: Ife559125850277865451f92ea862e7ec47fc9da7
    Cherry-picked from master
    Signed-off-by: Kevin Rocard <krocard@google.com>

--- gx.mk
+++ gx.mk
@@ -26,8 +26,8 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
-    android.hardware.audio@2.0-impl \
-    android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio@4.0-impl \
+    android.hardware.audio.effect@4.0-impl \
     android.hardware.soundtrigger@2.0-impl \
     audio.a2dp.default \
     audio.bluetooth.default \
